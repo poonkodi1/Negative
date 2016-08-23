a9c492e114171250ad058a9aa306db22bef661b4
poonkodi1 <poonkodidevaraj2013@gmail.com>
2016-08-23 18:14:38 +0530

    Add files via upload

---
+++ Volwel.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Volwel {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	   String  a;
+	   Scanner s=new Scanner(System.in);
+	   System.out.println("enter the");
+	   a=s.next();
+	   StringBuffer c=new StringBuffer(a);
+	   c.reverse();
+	   System.out.println(c);
+	   
+	   
+	  
+
+	}
+
+}
